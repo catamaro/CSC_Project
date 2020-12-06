7909284d079c5d014fb577b8e180f6be47841cce
catamaro <anapamaro98@gmail.com>
2020-12-06 18:32:56 +0000

    add client and server encryptions

--- Client1/client.cpp
+++ Client1/client.cpp
@@ -2,57 +2,78 @@
 
 using namespace std;
 
-string connect_to_server(string message){
+string load_string(string path)
+{
+    ifstream f(path);
+    string str;
+    if (f)
+    {
+        ostringstream ss;
+        ss << f.rdbuf();
+        str = ss.str();
+    }
+    f.close();
+
+    return str;
+}
+
+string connect_to_server(string message)
+{
 
     struct sockaddr_in server_addr;
     string reply(50, ' ');
 
-    int sock_fd  = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock_fd == -1){
+    int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_fd == -1)
+    {
         perror("socket ");
         exit(EXIT_FAILURE);
     }
-        
+
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(5000);
+    server_addr.sin_port = htons(5001);
     int err = inet_aton("127.0.0.1", &server_addr.sin_addr);
-    if(err == 0){
+    if (err == 0)
+    {
         perror("aton ");
         exit(EXIT_FAILURE);
     }
     // setup of server - creation of server connection to server
     err = connect(sock_fd, (const struct sockaddr *)&server_addr,
-                        sizeof(server_addr));
+                  sizeof(server_addr));
 
-    if(err == -1){
+    if (err == -1)
+    {
         perror("conn ");
         exit(EXIT_FAILURE);
     }
-    cout << "Just connected to the server";
+    cout << "\nJust connected to the server" << endl;
 
     auto bytes_sent = send(sock_fd, &message.front(), message.size(), 0);
 
-    cout << "\nClient sent: " << message << "\n";
+    cout << "\nClient sent: " << message << endl;
 
     auto bytes_received = recv(sock_fd, &reply.front(), reply.size(), 0);
 
-    cout << "\nClient received: " << reply << "\n";
+    cout << "\nClient received: " << reply << endl;
 
     return reply;
 }
 
-// function to encode values with Homomorphic Database Key  
-string encode_values(string message){
+// function to encode values with Homomorphic Database Key
+string encode_values(string message)
+{
     string message_values_encoded;
     return message_values_encoded;
-} 
+}
 
 // function to encode message with private and public key
-void encode_message(string message){
+void encode_message(string message)
+{
     string message_encoded;
 
     // create file with message to be encrypted
-    ofstream infile ("Files/message.txt");
+    ofstream infile("Files/message.txt");
 
     infile << message << endl;
 
@@ -62,106 +83,263 @@ void encode_message(string message){
     system("openssl x509 -pubkey -noout -in Files/Server-cert.crt > Files/Server-publ.pem\n");
     // generate random password file
     system("openssl rand -base64 32 > session.key\n");
-    // session key signed with server's public key
-    system("openssl rsautl -encrypt -pubin -inkey Files/Server-publ.pem -in session.key -out Files/Client1-session.key.enc\n"); 
-    
+    // session key encrypted with server's public key
+    system("openssl rsautl -encrypt -pubin -inkey Files/Server-publ.pem -in session.key -out Files/Client1-session.key.enc\n");
+
     // sign message with client's private key
-    system("openssl rsautl -sign -in Files/message.txt -inkey Files/Client1-priv.pem -out Files/message.sign\n"); 
+    system("openssl rsautl -sign -in Files/message.txt -inkey Files/Client1-priv.pem -out Files/message.sign\n");
+
     // encrypt signed message with session key
-    system("openssl enc -aes-256-cbc -pbkdf2 -salt -in Files/message.sign -out Files/Client1-message.enc -pass file:./session.key\n");   
+    system("openssl enc -aes-256-cbc -pbkdf2 -salt -in Files/message.sign -out Files/Client1-message.enc -pass file:./session.key\n");
 
-    // remove aux files
-    system("rm Files/message.sign\n rm Files/message.txt\n"); 
+    // remove unnecessary files
+    system("rm Files/message.sign\n rm Files/message.txt\n");
+
+    // moves encoded session-key, encoded message and certificate to server's folder
+    system("mv Files/Client1-message.enc ../Server/Files\n");
+    system("mv Files/Client1-session.key.enc ../Server/Files\n");
+    system("cp Files/Client1-cert.crt ../Server/Files\n");
+
+    // session key signed with client's private key (ver esta forma!!!!)
+    // system("openssl dgst -sha256 -sign Files/Client1-priv.pem -out sign.sha256 Files/Client1-session.key.enc")
 }
 
-// function to decode message with private and public key  
-string decode_message(string reply){
+// function to decode message with private and public key
+void decode_message()
+{
     string reply_decoded;
-    return reply_decoded;
+
+    // descrypt message with session key
+    system("openssl enc -d -aes-256-cbc -pbkdf2 -in Files/query_result.enc -out Files/query_result.txt -pass file:./session.key\n");
+
+    // remove unnecessary files: session key and encrypted query result
+    system("rm Files/query_result.enc\n rm session.key");
 }
 
-// function to decode values with Homomorphic Database Key  
-string decode_values(string reply){
+// function to decode values with Homomorphic Database Key
+string decode_values(string reply)
+{
     string reply_values_decoded;
     return reply_values_decoded;
-} 
+}
 
 // funciton to make it easy to write the query
-// not important 
-string create_command(int input){
+string create_query(int input_opt)
+{
+    int input;
+    string tablename, col_name, col_val, col_op, row_num;
+    string comm, comm1;
+
+    if(input_opt == 1){
+        cout << "Input message: ";
+        cin.ignore();
+        getline(cin, comm);
+        return comm;
+    }
+
+    cout << "Choose Query (1 to 6): ";
+    cin >> input;
+
     switch (input){
     case 1:
-        /* code */
+        cout << "Table Name ('end' to terminate): ";
+        cin >> tablename;
+
+        comm = "CREATE TABLE ";
+        comm.append(tablename);
+        comm.append(" (");
+
+        // construct query
+        while (true)
+        {
+            cout << "Column Name: ";
+            cin >> col_name;
+
+            if(col_name.compare("end") == 0) break;
+
+            comm.append(col_name);
+            comm.append(" ");
+        }
+        comm.append(")");
+
         break;
     case 2:
-        /* code */
+        cout << "Table Name ('end' to terminate): ";
+        cin >> tablename;
+
+        comm = "INSERT INTO TABLE  ";
+        comm.append(tablename);
+        comm.append(" (");
+
+        comm1 = "VALUES ";
+
+        // construct query
+        while(true)
+        {
+            cout << "Column Name: ";
+            cin >> col_name;
+            if(col_name.compare("end") == 0) break;
+
+            cout << "Column Value: ";
+            cin >> col_val;
+
+            comm.append(col_name);
+            comm.append(" ");
+            comm1.append(col_val);
+            comm1.append(" ");
+        }
+        comm.append(") ");
+        comm.append(comm1);
+
+        cout << comm << endl;
+
         break;
     case 3:
-        /* code */
+        cout << "Table Name: ";
+        cin >> tablename;
+
+        cout << "Row Number: ";
+        cin >> row_num;
+
+        comm = "DELETE ";
+        comm.append(row_num);
+        comm.append("FROM ");
+        comm.append(tablename);
+        
         break;
     case 4:
-        /* code */
+        cout << "Table Name: ";
+        cin >> tablename;
+
+        cout << "Row Number: ";
+        cin >> row_num;
+
+        comm = "SELECT ROW ";
+        comm.append(row_num);
+        comm.append("FROM ");
+        comm.append(tablename);
+        
         break;
     case 5:
-        /* code */
+        cout << "Table Name('end' to terminate): ";
+        cin >> tablename;
+
+        comm = "SELECT ";
+        comm.append(row_num);
+        while(true)
+        {
+            cout << "Column Name: ";
+            cin >> col_name;
+            if(col_name.compare("end") == 0) break;
+
+            comm.append(col_name);
+            comm.append(" ");
+        }
+
+        comm.append("FROM ");
+        comm.append(tablename);
+        comm.append(" WHERE ");
+
+        while(true)
+        {
+            cout << "\nColumn Name: ";
+            cin >> col_name;
+            if(col_name.compare("end") == 0) break;
+            cout << "Operand: ";
+            cin >> col_op;
+            cout << "Column Value: ";
+            cin >> col_val;
+
+            comm.append(col_name);
+            comm.append(col_op);
+            comm.append(col_val);
+            comm.append(" ");
+        }
         break;
     case 6:
-        /* code */
-        break;
-    default:
+        cout << "Table Name('end' to terminate): ";
+        cin >> tablename;
+
+        cout << "Column Name: ";
+        cin >> col_name;
+
+        comm = "SELECT SUM(";
+        comm.append(col_name);
+        comm.append(") FROM ");      
+        comm.append(tablename);
+        comm.append(" WHERE ");
+
+        while(true)
+        {
+            cout << "\nColumn Name: ";
+            cin >> col_name;
+            if(col_name.compare("end") == 0) break;
+            cout << "Operand: ";
+            cin >> col_op;
+            cout << "Column Value: ";
+            cin >> col_val;
+
+            comm.append(col_name);
+            comm.append(col_op);
+            comm.append(col_val);
+            comm.append(" ");
+        }
         break;
     }
 
-    return NULL;
+    cout << "\nCommand: " << comm << endl;
+    return comm;
 }
 
+void print_commands(){
+    cout << "+-------------------------------------------------------------------------------------------------------------------------------------+" << endl;
+    cout << "| Commands                   | Syntax                                                                                                 |" << endl;
+    cout << "+----------------------------+--------------------------------------------------------------------------------------------------------+" << endl;
+    cout << "| 1. Create new table        | CREATE TABLE tablename (col1name, col2name, …, colNname)                                               |" << endl;
+    cout << "| 2. Insert row in table     | INSERT INTO TABLE tablename (col1name, … , colNname) VALUES (value1, .., valueN)                       |" << endl;
+    cout << "| 3. Delete row from table   | DELETE linenum FROM tablename                                                                          |" << endl;
+    cout << "| 4. Select row from table   | SELECT ROW linenum FROM tablename                                                                      |" << endl;
+    cout << "| 5. Query table             | SELECT col1name, .., colNname FROM tablename WHERE col1name =|<|> value1 AND|OR col2name =|<|> value2  |" << endl;
+    cout << "| 6. Sum column              | SELECT SUM(colname) FROM tablename WHERE col1name =|<|> value AND|OR col2name =|<|> value              |" << endl;
+    cout << "| 7. Multiply column(no need)| SELECT MULT(colname) FROM tablename WHERE col1name =|<|> value AND|OR col2name =|<|> value             |" << endl;
+    cout << "+----------------------------+--------------------------------------------------------------------------------------------------------+" << endl;
+}
 
-int main(int argc, char* argv[]){
-    cout << "Welcome! To access and chage the database choose from the following commands:\n" << endl;
+void print_query_result(){
 
-    cout << "+------------------------------------------------------------------------------------------------------------------------------------------+" << endl;
-    cout << "| Commands                   | Syntax                                                                                                      |" << endl;
-    cout << "+----------------------------+-------------------------------------------------------------------------------------------------------------+" << endl;
-    cout << "| 1. Create new table        | CREATE TABLE tablename (col1name, col2name, …, colNname)                                                    |" << endl;
-    cout << "| 2. Insert row in table     | INSERT INTO TABLE (col1name, … , colNname) VALUES (value1, .., valueN)                                      |" << endl;
-    cout << "| 3. Delete row from table   | DELETE FROM tablename WHERE col1name =|<|>value1 AND|OR col2name =|<|> value2                               |" << endl;
-    cout << "| 4. Query table             | SELECT col1name, .., colNname FROM tablename WHERE col1name =|<|> value1 AND|OR col2name =|<|> value2       |" << endl;
-    cout << "| 5. Sum column              | SELECT SUM(colname) FROM tablename WHERE col1name =|<|> value AND|OR col2name =|<|> value                   |" << endl;
-    cout << "| 6. Multiply column         | SELECT MULT(colname) FROM tablename WHERE col1name =|<|> value AND|OR col2name =|<|> value                  |" << endl;
-    cout << "+----------------------------+-------------------------------------------------------------------------------------------------------------+" << endl;
+}
 
-    //int input;
-    //cin >> input;
-    //create_command();
+int main(int argc, char *argv[])
+{
+    cout << "Welcome! To access and change the database choose from the following commands\n"
+         << endl;
 
     bool run = true;
 
-    while(run){
-        cout << "Input your message" << endl;
+    string message, message_values_encoded, message_encoded;
+    string reply, reply_decoded, reply_values_decoded;
+    int input_opt;
 
-        string message, message_values_encoded, message_encoded;
-        string reply, reply_unecoded, reply_values_unecoded;
+    while (run)
+    {
+        // prints the server's API possible commands
+        print_commands();
+        
+        cout << "Construct Query (0), Input by Hand (1): ";
+        cin >> input_opt;
 
-        getline(cin, message);
-        if(message.compare("exit") == 0){
-            run = false;
-            continue;
-        }
-
+        message = create_query(input_opt);
+        
         //message_values_encoded = encode_values(message);
-        encode_message(message);
-        cout << "message was encoded"<<endl;
 
-        system("mv Files/Client1-message.enc ../Server/Files\n");
-        system("mv Files/Client1-session.key.enc ../Server/Files\n");
-        system("cp Files/Client1-cert.crt ../Server/Files\n");
+        encode_message(message);
 
         reply = connect_to_server("Client1");
 
-        //reply_unecoded = decode_message(reply);
-        //reply_values_unecoded = decode_values(reply_unecoded);
+        decode_message();
 
-        //cout << "Query result: " << reply_values_unecoded << endl;
-    }   
+        // reply_values_unecoded = decode_values(reply_unecoded);
+
+        // print_query_result();
+    }
 }
-
--- Server/server.cpp
+++ Server/server.cpp
@@ -34,13 +34,45 @@ void decode_message(string name){
     const char * run_comm2 = comm2.c_str();
     system(run_comm2);
 
-    // remove aux files
-    system("rm Files/Client1-message.enc\n rm Files/Client1-session.key.enc");
+    // remove unecessary files
+    string comm3("rm Files/");
+    comm3.append(name);
+    comm3.append("-message.enc\n rm Files/");
+    comm3.append(name);
+    comm3.append("-session.key.enc\n");
+
+    const char * run_comm3 = comm3.c_str();
+    system(run_comm3);
+}
+
+void encode_message(string query_result, string name){
+    
+    // create file with message to be encrypted
+    ofstream infile("Files/query_result.txt");
+
+    infile << query_result << endl;
+
+    infile.close();
+
+    // encrypt query result with session key
+    system("openssl enc -aes-256-cbc -pbkdf2 -salt -in Files/query_result.txt -out Files/query_result.enc -pass file:./session.key\n");
+
+    // moves encoded file to client folder
+    string comm("mv Files/query_result.enc ../");
+    comm.append(name);
+    comm.append("/Files\n");
+
+    const char * run_comm = comm.c_str();
+    system(run_comm);
+
+    // remove unnecessary files: session key and plain-text message
+    system("rm session.key\n rm Files/query_result.txt");
 }
 
 string verify_client_signature(string name){
 
     string message_decoded;
+
     // extract client's public key from certificate
     string comm("openssl x509 -pubkey -noout -in Files/");
     comm.append(name);
@@ -49,11 +81,14 @@ string verify_client_signature(string name){
     const char * run_comm = comm.c_str();
     system(run_comm);
 
+    // verify signature and extract message
     system("openssl rsautl -verify -inkey Files/Client-publ.pem -in Files/message.sign -pubin > Files/message.txt\n");
 
+    // import message into variable
     message_decoded = load_string("Files/message.txt");
-    cout << "message decoded: " << message_decoded << endl;
+    cout << "\nMessage decoded: " << message_decoded << endl;
         
+    // remove unecessary files
     system("rm Files/Client-publ.pem\n rm Files/message.sign\n");
 
     return message_decoded;
@@ -73,7 +108,7 @@ bool verify_root_CA()
 		if(root_id.compare("CSC-4\n") == 0)
 		{
 			root.close();
-			system("rm Files/root_id.txt");
+			system("rm Files/root_id.txt\n");
 			return true;
 		}
 		else
@@ -99,48 +134,157 @@ void create_database(){
 
 }
 
-void create_table(string name){
-
+int check_exists_table(string name){
+    
     string tbl("cd Encrypted_Database\n[ -d '");
     tbl.append(name);
     tbl.append("' ]");
     const char * table = tbl.c_str();
-
     if(system(table)){
-        cout << "table1 not exists !\n";
-        string tbl("cd Encrypted_Database\n mkdir table1");
+        string tbl("cd Encrypted_Database\n mkdir ");
+        tbl.append(name);
         const char * table = tbl.c_str();
         system(table);
-        cout << "created table !\n";
-
+        cout << "Created table!\n";
+        return 1;
     }
-    else
-    {
-        cout << "table1  exists !\n";
+    else{
+        cout << "Table already exists !\n";
+        return 0;
     }
-
 }
-void execute_query(string message_decoded){
-    if (message_decoded.find("CREATE") == 0) {
-            cout << "found CREATE!" << '\n';
-            create_table("table1");
+
+void create_clients_file(string name){
+    string filename ("");
+    filename.append(name);
+    filename.append("_clients.txt");
+    
+    //directory to create the file
+    string path1("/home/catarinamaro/Documents/Técnico/Cripto/Projeto/CSC_Project/Server/Encrypted_Database/");
+    path1.append(name);
+    path1.append("/");
+    path1.append(filename);
+
+    ofstream myfile(path1);
+
+    string data("Clients that have access to this table:");
+    myfile << data;
+    myfile.close();
+}
+
+void create_column(string name, string column){
+    string tbl("cd Encrypted_Database\n cd ");
+    tbl.append(name);
+    tbl.append("\n");
+    tbl.append(" mkdir ");
+    tbl.append(column);
+    tbl.append("\n");
+    const char * col = tbl.c_str();
+    system(col);
+
+    string filename ("");
+    filename.append(column);
+    filename.append("_data.txt");
+    //directory to create the file
+    string path1("/mnt/d/Drive_PC/IST/Mestrado/Cripto/Project/OurProject/Server/Encrypted_Database/");
+    path1.append(name);
+    path1.append("/");
+    path1.append(column);
+    path1.append("/");
+    path1.append(filename);
+    ofstream myfile(path1);
+    //myfile.open(filename);
+    string data("INT");
+    myfile << data;
+    myfile.close();
+}
+
+int create_table(string message){
+
+    string word = "";
+    string name = "";
+    int i = 1;
+    int j = 0;
+    int get_col = 0;
+    int type = 0;
+    char colnames[10][10] = {"", "", "", "", "", "", "","", "", "" }; //maximum 10 columns and 10 size
+    //Get name of the table and the columns
+    for (auto x : message) 
+    {
+        if( x == '('){
+            word = "";
+            get_col = 1;
+        }
+        else if(x == ')') break;
+
+        else if (x == ' ')
+        {
+            if(i == 3){
+                cout << "Table name is " + word << endl;
+                name = word;
+                i = 100;
+            }
+            if(get_col){
+                //const char * coluna = word.c_str();
+                strcpy(colnames[j], word.c_str());
+                j += 1;
+                word = "";
+            }
+            word = "";
+            i += 1;
+        }
+        else word = word + x;
+    }
+    
+    if (i == 3){
+        cout << "Table name is " + word << endl;
+        name = word;
+    }
+    //remove \n
+    if (!name.empty() && name[name.length()-1] == '\n') name.erase(name.length()-1);
+
+    // Check if table already exists, if not, create
+    if(check_exists_table(name) == 0){
+        //table already exists
+        return 0;
+    }
+
+    create_clients_file(name);
+    //Creating name of the file that contains clients ID
+
+    for (int k = 0; k<10; k++){
+        if(strcmp(colnames[k], "") != 0){
+            cout << colnames[k] << endl;
+            create_column(name, colnames[k]);
+        }
+    }    
+    return 1;
+}
+
+string execute_query(string message_decoded){
+    if (message_decoded.find("CREATE TABLE") == 0) {
+        cout << "Found CREATE TABLE!" << '\n';
+        create_table(message_decoded);
     }
     else if (message_decoded.find("INSERT") == 0)
     {
-        cout << "found INSERT!" << '\n';
+        cout << "Found INSERT!" << '\n';
     }
     else if (message_decoded.find("DELETE") == 0)
     {
-        cout << "found DELETE!" << '\n';
+        cout << "Found DELETE!" << '\n';
     }
     else if (message_decoded.find("SELECT") == 0)
     {
-        cout << "found SELECT!" << '\n';
+        cout << "Found SELECT!" << '\n';
     }
     else{
         cout << "Command not found!" << '\n';
     }
+
+    return "return query result?";
 }
+
 int main(int argc, char* argv[]){
 
     /************************************ setup socket ************************************/
@@ -158,7 +302,7 @@ int main(int argc, char* argv[]){
 
 	local_addr.sin_family = AF_INET;
 	local_addr.sin_addr.s_addr = INADDR_ANY;
-	local_addr.sin_port = htons(5000);
+	local_addr.sin_port = htons(5001);
 	int err = bind(server_fd, (struct sockaddr *)&local_addr,
 				   sizeof(local_addr));
 	if (err == -1)
@@ -177,9 +321,8 @@ int main(int argc, char* argv[]){
 
     /*********************************** accept messages **********************************/
     bool run = true;
-    string message(25, ' '); // stores the client name
-    string reply = "finished"; 
-    string message_decoded;
+    string client_name(25, ' '), reply = "finished"; // stores the client name
+    string message_decoded, query_result;
 
     while (run) {
         // accepting messages from clients
@@ -190,30 +333,33 @@ int main(int argc, char* argv[]){
         }
             
         // receives message that indicates wich client sent files
-        auto bytes_received = recv(newFD, &message.front(), message.length(), 0);
-        cout << "received message: " << message << endl;
+        auto bytes_received = recv(newFD, &client_name.front(), client_name.length(), 0);
+        cout << "\nReceived Query from " << client_name << endl;
 
         // remove \n from message
-        if (!message.empty()) message = message.substr(0, 7);
+        if (!client_name.empty()) client_name = client_name.substr(0, 7);
         
         // check if root certificate is valid
         run = verify_root_CA();
         if (run == false) continue;
         
-        // decode message and session key
-        decode_message(message);
+        // decode message and session key and saves it in folder
+        decode_message(client_name);
         // verify if message was really signed by the client
-        message_decoded = verify_client_signature(message);
+        message_decoded = verify_client_signature(client_name);
         
         // creates the directory that will store the tables
         create_database();
-        // execute the decrypted query with homomorphic encrypted values
-        execute_query(message_decoded);
+        // executes the decrypted query with homomorphic encrypted values
+        query_result = execute_query(message_decoded);
+
+        // encrypt with session key and move to client folder
+        encode_message(query_result, client_name);
 
         // message to alert client that the answer is already the files directory
         auto bytes_sent = send(newFD, &reply.front(), reply.length(), 0);
 
-        cout << "sent message: " << reply << endl;
+        cout << "\nSent message: " << reply << endl;
         // ends communication between client and server
         close(newFD);
     }
